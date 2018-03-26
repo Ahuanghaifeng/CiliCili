2f11c753a375fc9b4aca9176a487f7e176556bc3
Ahuanghaifeng <1490738477@qq.com>
2018-03-26 18:01:02 +0800

    革命尚未成功

--- app/src/main/java/com/cilicili/cilicili/IndexFragment.java
+++ app/src/main/java/com/cilicili/cilicili/IndexFragment.java
@@ -1,17 +1,29 @@
 package com.cilicili.cilicili;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.cilicili.cilicili.bean.IndexBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by haifeng on 2018/3/23.
  */
 
-public class IndexFragment extends Fragment{
+public class IndexFragment extends Fragment implements BaseQuickAdapter.RequestLoadMoreListener{
+
+    private RecyclerView recyclerView;
 
     public static IndexFragment newInstance() {
         
@@ -31,6 +43,32 @@ public class IndexFragment extends Fragment{
     }
 
     public void initView(View view){
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        IndexFragmentAdapter adapter = new IndexFragmentAdapter(R.layout.item_index);
+        recyclerView.setAdapter(adapter);
+        List<IndexBean> mData = new ArrayList<>();
+        mData.add(new IndexBean());
+        mData.add(new IndexBean());
+        mData.add(new IndexBean());
+        adapter.setNewData(mData);
+        adapter.setOnLoadMoreListener(this,recyclerView);
+    }
 
+    @Override
+    public void onLoadMoreRequested() {
+
+    }
+
+    private class IndexFragmentAdapter extends BaseQuickAdapter<IndexBean,BaseViewHolder>{
+
+        public IndexFragmentAdapter(@LayoutRes int layoutResId) {
+            super(layoutResId);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, IndexBean item) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/cilicili/cilicili/bean/IndexBean.java
@@ -0,0 +1,8 @@
+package com.cilicili.cilicili.bean;
+
+/**
+ * Created by haifeng on 2018/3/26.
+ */
+
+public class IndexBean {
+}
